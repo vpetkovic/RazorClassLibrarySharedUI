1902046c159e76d5421daec140a15ae13ebe5d34
Vojislav Petkovic <vpetkovic@outlook.com>
2021-05-29 23:25:48 -0400

    🚀 Added Demo Project Files

---
+++ src/Demo.SharedUI/StartupHelpers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo.SharedUI
+{
+    public static class StartupHelpers
+    {
+        public static void AddSharedUI(this IServiceCollection services)
+        {
+            services.ConfigureOptions(typeof(SharedUIConfigureOptions));
+        }
+    }
+}
