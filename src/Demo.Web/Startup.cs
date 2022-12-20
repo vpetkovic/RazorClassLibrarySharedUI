131b6b5643b76fc5bf300d35be89542307b8d971
Vojislav Petkovic <vpetkovic@outlook.com>
2022-12-19 20:37:03 -0500

    👌 Improved & Upgraded to .NET7
    - Added Blazor Server & WASM
    - Updated README.md

--- src/Demo.Web/Startup.cs
+++ src/Demo.Web/Startup.cs
@@ -25,7 +25,6 @@ namespace Demo.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddSharedUI();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
